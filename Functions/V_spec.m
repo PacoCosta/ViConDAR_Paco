b2a636684cfb61f5e3dbb922a318f75ee41762b3
Francisco Costa <costa@ifb.uni-stuttgart.de>
2022-01-30 18:12:13 +0100

    Update V_spec.m

--- Functions/V_spec.m
+++ Functions/V_spec.m
@@ -49,10 +49,8 @@ for ind_bin = floorS1:binwidth:ceilS1 %13:binwidth:16 %
     for iii = 1:length(VFinalTotal_TimeInt3)
         if (ind_bin<VFinalTotal_TimeInt3(iii)) && (VFinalTotal_TimeInt3(iii) <=ind_bin+binwidth)
             %             DSpectrum(iv,i_bin)      = VFinalTotal_TimeInt3(iii)*WeightFun(iii)*abs(distan(iii));
-            DSpectrum(iv,i_bin)      = WeightFun(iii)*abs(distan(iii));
-            
+            DSpectrum(iv,i_bin)      = WeightFun(iii)*abs(distan(iii));            
         else
-            %         Vel_sprectrum(iv,i_bin) = 0;
             DSpectrum(iv,i_bin)      = 0;
         end
         iv=iv+1;
@@ -61,7 +59,7 @@ for ind_bin = floorS1:binwidth:ceilS1 %13:binwidth:16 %
 end
 SumSpectrum=sum(DSpectrum);
 Fvals = cumsum(SumSpectrum.*dt);
-F = spline(binrang+binwidth/2,Fvals);
+F = spline(binrang+binwidth/2,[0,Fvals,0]);
 DF = fnder(F);  % computes its first derivative
 
 % [counts,V_levels]=histcounts(nonzeros(VFinalTotal_TimeInt3'.*WeightFun),'BinWidth',bin_width);
@@ -71,26 +69,20 @@ DF = fnder(F);  % computes its first derivative
 
 for ind_peak_method=1:size(input.peak_detection_method,2)
     if strcmpi(input.peak_detection_method{ind_peak_method},"median")
-        %Median
-        [g,b]=unique(Fvals/max(Fvals));
-        
-        ff=fit (g',t(b)','linearinterp');
-        VFinalTotal_Time = ff(.5);
+        %Median: Median is calculated as the velocity value where the cdf =0.5
+        [g,b]=unique(Fvals/max(Fvals)); % Take unique values to calculate the interpolation. The cdf is normalised using the maximum        
+        ff=fit (g',t(b)','linearinterp'); %Fit the cdf to get the value in cdf=0.5
+        VFinalTotal_Time = ff(.5); % Value in cdf=.5
     elseif strcmpi (input.peak_detection_method{ind_peak_method},"maximum")
-        %Max
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %Max: Computes the maximum of the histogram.
         dd=fnplt(DF, 'g', 2);
         [~,n_max]=(max(dd(2,:)));
-        VFinalTotal_Time=dd(1,n_max);
-        
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        
+        VFinalTotal_Time=dd(1,n_max);        
     elseif strcmpi (input.peak_detection_method{ind_peak_method},"centroid")
         %Centroid
-        VFinalTotal_Time=sum(SumSpectrum.*binrang)/sum(SumSpectrum);
-        
+        VFinalTotal_Time=sum(SumSpectrum.*binrang)/sum(SumSpectrum);        
     elseif strcmpi(input.peak_detection_method{ind_peak_method},"mean")
-        %Mean
+        %Mean: Computes the mean of the values whithin the probe volume
         VFinalTotal_Time = sum(WeightFun'.*VFinalTotal_TimeInt3)/sum(WeightFun); %#ok<*AGROW>
     end
 end
